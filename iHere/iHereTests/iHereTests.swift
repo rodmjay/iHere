6508f55b975799597e8a3c6a06349bcae853711b
Nathan Larson <nathanlarson@AST-Alpha-MacBook-Pro.local>
2015-06-11 10:05:06 -0600

    initial commit

---
+++ iHere/iHereTests/iHereTests.swift
@@ -0,0 +1,36 @@
+//
+//  iHereTests.swift
+//  iHereTests
+//
+//  Created by Nathan Larson on 6/11/15.
+//  Copyright (c) 2015 Nathan Larson. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class iHereTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
