a12d04e27002bc103f36461ccbeaa3158379e3ed
Nathan Larson <nathanlarson@AST-Alpha-MacBook-Pro.local>
2015-06-12 11:03:27 -0600

    added lib file

---
+++ BeaconLib/BeaconProxLib/BeaconProxLib/BeaconProxLib.h
@@ -0,0 +1,22 @@
+//
+//  BeaconProxLib.h
+//  BeaconProxLib
+//
+//  Created by Nathan Larson on 6/12/15.
+//  Copyright (c) 2015 Nathan Larson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BeaconProxLib : NSObject
+
+// start scanning
+// stop scanning
+// start advertising
+// stop advertising
+
+// List of peripheral names
+// Create CBUUID
+
+
+@end
---
+++ BeaconLib/BeaconProxLib/BeaconProxLib/BeaconProxLib.m
@@ -0,0 +1,13 @@
+//
+//  BeaconProxLib.m
+//  BeaconProxLib
+//
+//  Created by Nathan Larson on 6/12/15.
+//  Copyright (c) 2015 Nathan Larson. All rights reserved.
+//
+
+#import "BeaconProxLib.h"
+
+@implementation BeaconProxLib
+
+@end
